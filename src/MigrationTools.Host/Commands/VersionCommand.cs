00781392406177414f4d63c7850cd13b7db5f45e
Sébastien Bouchex Bellomié <Sebastien.BouchexBellomie@philips.com>
2026-01-19 11:44:22 +0100

    adress comments

--- src/MigrationTools.Host/Commands/VersionCommand.cs
+++ src/MigrationTools.Host/Commands/VersionCommand.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -6,8 +6,6 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using MigrationTools.Host.Services;
 using MigrationTools.Services;
-using OpenTelemetry.Trace;
-using Serilog;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
@@ -26,34 +24,27 @@ namespace MigrationTools.Host.Commands
             ITelemetryLogger telemetryLogger,
             IMigrationToolVersion migrationToolVersion,
             IConfiguration configuration,
-            ActivitySource activitySource) 
-            : base(appLifetime, services, detectOnlineService, detectVersionService, logger, telemetryLogger, migrationToolVersion, configuration, activitySource)
+            ActivitySource activitySource)
+            : base(appLifetime, services, detectOnlineService, detectVersionService, logger, telemetryLogger,
+                migrationToolVersion, configuration, activitySource)
         {
             _migrationToolVersion = migrationToolVersion;
         }
 
         internal override Task<int> ExecuteInternalAsync(CommandContext context, VersionCommandSettings settings)
         {
-            try
+            var versionInfo = _migrationToolVersion.GetRunningVersion();
+
+            AnsiConsole.MarkupLine($"[bold cyan]Version:[/] {Markup.Escape(versionInfo.versionString)}");
+
+            if (versionInfo.version.Major == 0)
             {
-                var versionInfo = _migrationToolVersion.GetRunningVersion();
-                
-                AnsiConsole.MarkupLine($"[bold cyan]Version:[/] {versionInfo.versionString}");
-                
-                if (versionInfo.version.Major == 0)
-                {
-                    AnsiConsole.MarkupLine($"[dim]Git Tag:[/] {ThisAssembly.Git.Tag}");
-                    AnsiConsole.MarkupLine($"[dim]Git Branch:[/] {ThisAssembly.Git.Branch}");
-                    AnsiConsole.MarkupLine($"[dim]Git Commits:[/] {ThisAssembly.Git.Commits}");
-                }
-                
-                return Task.FromResult(0);
+                AnsiConsole.MarkupLine($"[dim]Git Tag:[/] {Markup.Escape(ThisAssembly.Git.Tag)}");
+                AnsiConsole.MarkupLine($"[dim]Git Branch:[/] {Markup.Escape(ThisAssembly.Git.Branch)}");
+                AnsiConsole.MarkupLine($"[dim]Git Commits:[/] {Markup.Escape(ThisAssembly.Git.Commits)}");
             }
-            catch (Exception ex)
-            {
-                CommandActivity.RecordException(ex);
-                return Task.FromResult(1);
-            }
+
+            return Task.FromResult(0);
         }
     }
 }
